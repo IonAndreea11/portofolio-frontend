d4713db94f1df3a4b467007f25dd8de2c004805d
IonAndreea11 <ion.andreea@yahoo.com>
2025-12-25 13:19:07 +0100

    New Settings

--- src/components/HomeContact.jsx
+++ src/components/HomeContact.jsx
@@ -1,8 +1,8 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "../styles/HomeContact.css";
 
-function HomeContact(){
-    const [formData, setFormData] = useState({
+function HomeContact() {
+  const [formData, setFormData] = useState({
     name: "",
     email: "",
     message: "",
@@ -18,7 +18,7 @@ function HomeContact(){
     setStatus("Sending...");
 
     try {
-      const res = await fetch("http://localhost:5000/api/contact", {
+      const res = await fetch(`${process.env.REACT_APP_API_URL}/api/contact`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
@@ -36,50 +36,45 @@ function HomeContact(){
     }
   };
 
-
-  return(
+  return (
     <section className="home-contact-section" id="contact">
-        <h2 className="home-contact-title">Contact Me</h2>
-        <p className="home-contact-subtitle">
-            Feel free to reach out if you’d like to collaborate or just say hello!
-        </p>
+      <h2 className="home-contact-title">Contact Me</h2>
+      <p className="home-contact-subtitle">
+        Feel free to reach out if you’d like to collaborate or just say hello!
+      </p>
 
-        <form className="home-contact-form" onSubmit={handleSubmit}>
-            <input 
-            type="text"
-            name="name"
-            placeholder="Your Name"
-            value ={formData.name}
-            onChange={handleChange}
-            required
-            />
-            <input 
-            type="email"
-            name="email"
-            placeholder="Your Email"
-            value ={formData.email}
-            onChange={handleChange}
-            required
-            />
-            <textarea
-            name="message"
-            placeholder="Your Message"
-            rows="7"
-            value={formData.message}
-            onChange={handleChange}
-            required
-            ></textarea>
-            <button type="submit" className="home-contact-btn">
-                Send Message
-            </button>
-        </form>
-        {status && 
-        <p className="home-contact-status">
-            {status}
-        </p>
-        }
+      <form className="home-contact-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Your Name"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Your Email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+        <textarea
+          name="message"
+          placeholder="Your Message"
+          rows="7"
+          value={formData.message}
+          onChange={handleChange}
+          required
+        ></textarea>
+        <button type="submit" className="home-contact-btn">
+          Send Message
+        </button>
+      </form>
+      {status && <p className="home-contact-status">{status}</p>}
     </section>
-  )
+  );
 }
 
 export default HomeContact;
